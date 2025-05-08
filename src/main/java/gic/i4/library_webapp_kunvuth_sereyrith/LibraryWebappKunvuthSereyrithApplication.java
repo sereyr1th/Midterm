d62d7123c16761d8277de2a75d0a520af80c159f
Rith <mobshigeo99@gmail.com>
2025-05-08 08:27:15 +0700

    Initial commit for Spring boot

---
+++ src/main/java/gic/i4/library_webapp_kunvuth_sereyrith/LibraryWebappKunvuthSereyrithApplication.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_kunvuth_sereyrith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebappKunvuthSereyrithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebappKunvuthSereyrithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/i4/library_webapp_kunvuth_sereyrith/LibraryWebappKunvuthSereyrithApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.library_webapp_kunvuth_sereyrith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebappKunvuthSereyrithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
